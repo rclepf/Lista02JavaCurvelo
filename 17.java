80d9c58dd85a1673d6363cd67334fafdc3c31845
Rodolfo Clepf <68128390+rclepf@users.noreply.github.com>
2025-10-06 20:01:28 -0300

    Create 17.java

---
+++ 17.java
@@ -0,0 +1,13 @@
+public class Solucao17 {
+    public static void main(String[] args) {
+        int n1 = 0, n2 = 1, n3, count = 10;
+        System.out.print(n1 + " " + n2); // Imprime 0 e 1
+
+        for (int i = 2; i < count; ++i) {
+            n3 = n1 + n2;
+            System.out.print(" " + n3);
+            n1 = n2;
+            n2 = n3;
+        }
+    }
+}
