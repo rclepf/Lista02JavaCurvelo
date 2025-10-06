215e4e997d80f6c1820890a8c56fcd9cdb228fb9
Rodolfo Clepf <68128390+rclepf@users.noreply.github.com>
2025-10-06 20:03:33 -0300

    Create 27.java

---
+++ 27.java
@@ -0,0 +1,13 @@
+public class Solucao27 {
+    public static void main(String[] args) {
+        int[][] matriz = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int soma = 0;
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                soma += matriz[i][j];
+            }
+        }
+        System.out.println("A soma de todos os elementos da matriz é: " + soma);
+    }
+}
